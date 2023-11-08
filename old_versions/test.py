1c55999b1e881054c80d2c2c2449c15f93a74bc7
Francis Lewis <francislewis@users.noreply.github.com>
2023-11-08 17:50:03 +0000

    remove old files

--- old_versions/test.py
+++
@@ -1,7 +0,0 @@
-from database import fetch_property_details_from_db
-
-ids = [1869082, 1811237, 1871704, 1845781, 1874378]
-
-for id in ids:
-    property = fetch_property_details_from_db(id)
-    print(property)
